ea1d512b6c307950354f0da5a6caff63a070ccfe
milenarismo <milenasimoeslabenu@gmail.com>
2022-10-25 11:02:10 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+import { useState } from "react";
 
 const GlobalStyled = createGlobalStyle`
   *{
@@ -14,14 +15,48 @@ const MainContainer = styled.main`
 `
 
 function App() {
+  const [tela, setTela] = useState(1)
+  const mudarTela = () => setTela(2)
 
-  return (
-    <MainContainer >
-      <GlobalStyled />
-      <TelaLogin />
-      <TelaCadastro />
-    </MainContainer>
-  );
-}
+
+ 
+  switch(tela){
+   case 1:
+     return (
+     <MainContainer >
+       <GlobalStyled />
+       <TelaLogin mudarTela={()=>setTela(2)} />
+     </MainContainer>
+   );
+
+ case 2 :
+   return(
+  <MainContainer >
+   <GlobalStyled />
+   <TelaCadastro mudarTela={()=>setTela(1)}/>
+ </MainContainer>);
+
+ default : 
+ return (
+   <MainContainer >
+  <GlobalStyled />
+  <h1>Algo deu errado</h1>
+   <TelaLogin />
+ </MainContainer>
+ );
+  
+ }}
+
+// return(
+//   <MainContainer >
+//   <GlobalStyled />
+//   {/* condição ? true : false */}
+//   {/* { tela === 1 ? <TelaLogin/> : <TelaCadastro/>} */}
+//   {/* curto circuito */}
+//   {/* {(tela && <TelaLogin/>) || <TelaCadastro/>} */}
+//   {/* se tela não for um valor falso dentro da coersão, então ele retorna o segundo valor */}
+// </MainContainer>
+// )
+
 
 export default App;
